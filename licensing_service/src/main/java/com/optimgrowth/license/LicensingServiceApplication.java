ddc8d9649dc1ce5cbca9c5c5d9188a453e9d79f1
coolabhays <abhaysp9955@gmail.com>
2022-09-01 18:58:40 +0530

    fix: fix spelling mistake

--- licensing_service/src/main/java/com/optimgrowth/license/LicensingServiceApplication.java
+++ licensing_service/src/main/java/com/optimgrowth/license/LicensingServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LisencingServiceApplication {
+public class LicensingServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LisencingServiceApplication.class, args);
+		SpringApplication.run(LicensingServiceApplication.class, args);
 	}
 
 }
