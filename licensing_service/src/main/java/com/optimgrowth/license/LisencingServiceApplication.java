87f0d1115b065b8f870263cdc2b8d8a7a826b68b
coolabhays <abhaysp9955@gmail.com>
2022-09-01 18:49:38 +0530

    feat: initial project commit for licensing service

---
+++ licensing_service/src/main/java/com/optimgrowth/license/LisencingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.optimgrowth.license;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LisencingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LisencingServiceApplication.class, args);
+	}
+
+}
---
+++ licensing_service/src/test/java/com/optimgrowth/license/LisencingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimgrowth.license;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LisencingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
