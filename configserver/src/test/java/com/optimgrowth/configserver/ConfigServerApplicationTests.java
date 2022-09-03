459a432f395728b8e1c7a6308693f09b3979c4cb
coolabhays <abhaysp9955@gmail.com>
2022-09-03 20:38:55 +0530

    feat: updated pom.xml for cloud config and docker usage

---
+++ configserver/src/test/java/com/optimgrowth/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.optimgrowth.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
