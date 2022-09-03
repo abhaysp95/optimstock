e147129b165f8ee1ff993964e7cbeb9a2cbee4be
coolabhays <abhaysp9955@gmail.com>
2022-09-03 21:31:51 +0530

    feat: make service behave for cloud config

--- configserver/src/main/java/com/optimgrowth/configserver/ConfigServerApplication.java
+++ configserver/src/main/java/com/optimgrowth/configserver/ConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.optimgrowth.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer  // enables the service as spring cloud config service
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
